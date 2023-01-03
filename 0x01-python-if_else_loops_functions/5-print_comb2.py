e9c8d1dc10b3bb5258d4157834c8055226e3ef7b
Mitchell Nwafor <cozymitch2624@gmail.com>
2023-01-03 20:14:14 +0000

    completed task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(0, 10):
-        print("{}".format(i), end="")
-        print("{}, ".format(j), end="")
+for i in range(0, 100):
+    if i == 99:
+        print("{}".format(i))
+    else:
+        print("{:02}".format(i), end=", ")
