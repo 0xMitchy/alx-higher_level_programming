fe034e608b788b80acad01714a5347121750985c
Mitchell Nwafor <cozymitch2624@gmail.com>
2023-01-08 16:06:03 +0000

    completed task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+    import sys
+
+    argcount = len(sys.argv) - 1
+    if argcount == 0:
+        print("0 arguments.")
+    elif argcount == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+
+    for i in range (argcount):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
